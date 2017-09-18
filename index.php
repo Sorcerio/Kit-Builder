720a91ab934abc54278977d31015795abb2d33de
maximombro <maximombro@gmail.com>
2017-09-18 13:09:49 -0400

    Standard setup

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<header>
+    <title>Kit Builder</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Brody.MC Media">
+    <meta name="description" content="Welcome to the Brody.MC Media kit builder. Here you can build a kit for your next airsoft or paintball game?">
+    <link rel="icon" type="image/png" href="images/favicon.png">
+    <link rel="stylesheet" type="text/css" href="css/mainStyle.css">
+    <link rel="stylesheet" type="text/css" href="assets/w3.css">
+</header>
+<body>
+    <h1>FIRST TIME AROUND</h1>
+</body>
+</html>
