59b60910a6902d5c1abc2785979676082b5912b3
maximombro <maximombro@gmail.com>
2017-09-18 14:00:41 -0400

    It's begining

--- index.php
+++ index.php
@@ -5,12 +5,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Brody.MC Media">
-    <meta name="description" content="Welcome to the Brody.MC Media kit builder. Here you can build a kit for your next airsoft or paintball game?">
+    <meta name="description" content="Welcome to the Brody.MC Media Kit Builder. Here you can build a kit for your next airsoft or paintball game?">
+    <meta name="tags" content="TO-DO">
     <link rel="icon" type="image/png" href="images/favicon.png">
     <link rel="stylesheet" type="text/css" href="css/mainStyle.css">
     <link rel="stylesheet" type="text/css" href="assets/w3.css">
 </header>
 <body>
-    <h1>FIRST TIME AROUND</h1>
+    <!-- Tool Bar -->
+    <div class="toolBar">
+        <ul>
+            <a href="#"><li>[ Equipment ]</li></a>
+            <a href="#"><li>Primary Weapon</li></a>
+            <a href="#"><li>Secondary Weapon</li></a>
+            <a href="#"><li>Extras</li></a>
+        </ul>
+    </div>
+
+    <!-- Page Contents -->
+    <div class="toolBar_otherSpacer">
+        <!-- Left Panel -->
+        <div class="main_LeftPanel">
+            <h1 class="main_Header">Select Type</h1>
+        </div>
+
+        <!-- Right Panel -->
+        <div class="main_RightPanel">
+            <h1 class="main_Header">Select Choice</h1>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+            </ul>
+        </div>
+    </div>
+    
+    <!-- Footer -->
+    <!-- <div>
+        <p>I'm the footer<p>
+    </div> -->
 </body>
+<script>
+</script>
 </html>
