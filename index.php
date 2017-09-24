fea091d387ee98c68b6bb26f0b1fda62b7c29fa1
maximombro <maximombro@gmail.com>
2017-09-24 11:53:15 -0400

    Added production license and tweaky breeky

--- index.php
+++ index.php
@@ -140,7 +140,8 @@
     <div class="footer">
         <ul class="footer_List">
             <li><p>Copyright under MIT License to Brody.MC Media and all personal aliases.</p></li>
-            <li><a href="LICENSE" title="Opens in a new tab." target="_blank"><p>View License.</p></a></li>
+            <!-- <li><a href="LICENSE" title="Opens in a new tab." target="_blank"><p>View License.</p></a></li> -->
+            <li><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><p>View License</p></a>.</li>
             <li><a href="credits.php" title="Opens in a new tab." target="_blank"><p>View Credits.</p></a></li>
             <li><a href="https://github.com/maximombro/Kit-Builder" title="Opens in a new tab." target="_blank"><p>View on Github.</p></a></li>
         </ul>
