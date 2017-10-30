395b6490f6015e06ff7f48db82e26046a51aa029
maximombro <maximombro@gmail.com>
2017-10-30 12:53:16 -0400

    Added the beginning of a disclaimer system

--- assets/footer.php
+++ assets/footer.php
@@ -4,6 +4,7 @@
         <!-- <li><a href="LICENSE" title="Opens in a new tab." target="_blank"><p>View License.</p></a></li> -->
         <li><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/" title="Opens in a new tab." target="_blank"><p>View License.</p></a>.</li>
         <li><a href="credits.php" title="Opens in a new tab." target="_blank"><p>View Credits.</p></a></li>
+        <li><a href="disclaimer.php" title="Opens in a new tab." target="_blank"><p>View Disclaimer.</p></a></li>
         <li><a href="https://github.com/maximombro/Kit-Builder" title="Opens in a new tab." target="_blank"><p>View on Github.</p></a></li>
         <li><a href="newDataForum.php" title="Opens in a new tab." target="_blank"><p>Help Submit New Data.</p></a></li>
     </ul>
---
+++ disclaimer.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<header>
+    <title>Kit Builder</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Brody.MC Media">
+    <meta name="description" content="Welcome to the Brody.MC Media Kit Builder. Here you can build a kit for your next airsoft or paintball game?">
+    <meta name="tags" content="TO-DO">
+    <link rel="icon" type="image/png" href="images/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="css/mainStyle.css">
+    <link rel="stylesheet" type="text/css" href="assets/w3.css">
+</header>
+<body>
+    <!-- Return Bar -->
+    <!-- <a href="index.php"><p style="text-align: center;">Return to Kit Builder</p></a> -->
+
+    <!-- Main Content -->
+    <div style="margin-left: 10px;">
+        <h3>Kit Builder Disclaimer</h3>
+        <p>
+        The Kit Builder is provided as is in its current form without any warrantee or guarantee.<br>
+        Any information we are using is public domain or we have permission to use it.<br>
+        <br>
+        The Kit Builder uses user input for much of its data and therefore may possess terms or phrases that some may find offensive.<br>
+        The Kit Builder is moderated, however, not everything can be caught right away. We do our best remove offensive material.<br>
+        As such, any user submitted content is not representative of views of Brody.MC Media, this tool's developers, or any of Brody.MC Media's branches.<br>
+        <br>
+        Have fun using the tool and remember that while we are on GitHub, we do have terms of use which can be found here: <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/" title="Opens in a new tab." target="_blank">License</a>.  
+        </p>
+    </div>
+
+    <!-- Footer -->
+    <?php
+        include("assets/footer.php");
+    ?>
+</body>
+<script>
+</script>
+</html>
